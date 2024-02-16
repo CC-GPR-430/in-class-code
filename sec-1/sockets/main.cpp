b638b87ce560fb3ee95a0f3354f41c46daed3d2d
Scott Barrett <sbarrett@champlain.edu>
2024-02-16 12:37:41 -0500

    add sec1 backoff example

--- sec-1/sockets/main.cpp
+++ sec-1/sockets/main.cpp
@@ -21,6 +21,89 @@ void do_client() {
 	// Send to this computer, which "127.0.0.1" always refers to.
 	Address server_addr("127.0.0.1", 10245);
 
+	// Want to wait some amount of time for a response, and if
+	// we don't get it, we assume it's not coming. So, we want
+	// to resend our message and wait again.
+
+	float wait_time = 0.1f;
+	while (true)
+	{
+		udp_sock.SetTimeout(wait_time);
+
+		char buffer[4096];
+		size_t nbytes_sent = udp_sock.SendTo(
+			msg_to_send.data(), msg_to_send.size(),
+			server_addr);
+		Address from_addr;
+		std::cout << "Waiting on recv()...\n";
+		int nbytes_recvd = udp_sock.RecvFrom(
+			buffer, sizeof(buffer),
+			from_addr);
+		
+		// With the timeout, RecvFrom() might return either because:
+		// - It received data successfully, or
+		// - A timeout occurred.
+		// We can tell which situation we're in by looking at
+		// the value returned.
+		// nbytes_recved == -1 => no data was received.
+		// nbytes_recved >  0  => nbytes_recved bytes were received.
+		if (nbytes_recvd == -1)
+		{
+			// For some reason, we didn't get data. We need to check
+			// what that reason was.
+			if (udp_sock.GetLastError() == Socket::Error::SOCKLIB_ETIMEDOUT)
+			{
+				// This means we timed out
+				// We want to try again!
+				// Resend / call recvfrom() again
+				std::cout << "Timed out!\n";
+				wait_time *= 2;
+				if (wait_time >= 5.0)
+				{
+					std::cout << "Exceeded max wait time. Giving up.\n";
+					break;
+				}
+			}
+			else
+			{
+				// Unknown error. Abort.
+				std::cout << "Unknown error occurred. Aborting.\n";
+			}
+		}
+		if (nbytes_recvd > 0)
+		{
+			std::cout << "Got a message! We're done here.\n";
+			// BUT WAIT! Who sent this data?! We need to confirm it's who
+			// we think it is!
+			// Couple options for how to test:
+			// 1. Check addresses for equality -- from_addr should be the same
+			//		as serv_addr
+			//** 2. Include some data with the client request that only the client
+			//      knows. So for example, send:
+			//    "REQUEST_ID|MESSAGE_BODY"
+			// Ex. "82734|Hi there server!"
+			//      REQUEST_ID should be *randomly generated* -- otherwise, an
+			//		interloper can figure out what the next request ID will be
+			//		and use that to masquerade as the server.
+			//		Then, when we receive a message, it should include the same request ID.
+			//		If not, we ignore it.
+			// 3. You can actually use connect() on UDP sockets!
+			//		This doesn't work the same as connect() with TCP, in the sense
+			//		that it doesn't offer any real guarantees. But, it will essentially
+			//		automatically do option (1) for you, so you don't have to do it
+			//		explicitly. That is, the socket will check the incoming address
+			//		for you, and make sure it matches the address specified in connect().
+			//		Also, when using connect(), you can use send() and recv(), not just
+			//		sendto() and recvfrom().
+			//
+			// NOTE that a determined attacker can circumvent ALL OF these techniques.
+			// When handling sensitive data, the only option is to use well-tested
+			// encryption techniques (which we'll cover later).
+			break;
+		}
+	}
+	return;
+
 	// SendTo() takes an extra parameter as compared to Send():
 	// the address to which the data should be sent.
 	// The same socket can send data to any address, unlike
@@ -40,11 +123,10 @@ void do_client() {
 	// last parameter. This address is the address from
 	// which the data arrived.
 	size_t nbytes_recvd = udp_sock.RecvFrom(buffer, sizeof(buffer), msg_from);
+	std::string msg_recvd(buffer, nbytes_recvd);
 
 	std::cout << "Received " << nbytes_recvd << " bytes from " << msg_from
-		<< ": '";
-	std::cout.write(buffer, nbytes_recvd);
-	std::cout << "'\n";
+		<< ": '" << msg_recvd << "'\n";
 }
 
 void do_server()
@@ -63,12 +145,12 @@ void do_server()
 	char buffer[4096];
 	Address from_address;
 	size_t nbytes_recvd = udp_sock.RecvFrom(buffer, sizeof(buffer), from_address);
+	std::string msg_recvd(buffer, nbytes_recvd);
 	std::cout << "Received " << nbytes_recvd << " bytes from " << from_address
-		<< ": '";
-	std::cout.write(buffer, nbytes_recvd);
-	std::cout << "'.\n";
+		<< ": '" << msg_recvd << "'.\n";
 
 	std::string msg_to_send("Hello! I'm so sad :)");
+
 	size_t nbytes_sent = udp_sock.SendTo(msg_to_send.data(), msg_to_send.size(), from_address);
 	std::cout << "Sent " << nbytes_sent << " bytes to " << from_address << ".\n";
 }
@@ -76,6 +158,7 @@ void do_server()
 int main(int argc, char *argv[]) {
 	SockLibInit();
 	defer _([]() {SockLibShutdown();});
+	srand(time(NULL));
 
 	// Run client if no args passed on command line;
 	// otherwise, run server.
