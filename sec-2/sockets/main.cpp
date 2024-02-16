63e4d256c0689f20a2b02da1673a5a4cd67e0b51
Scott Barrett <sbarrett@champlain.edu>
2024-02-16 12:38:39 -0500

    move sec-2 code to sec-2 folder

--- sec-2/sockets/main.cpp
+++ sec-2/sockets/main.cpp
@@ -2,126 +2,119 @@
 #include <sstream>
 #include <string>
 #include <time.h>
+#include <stdlib.h>
 
 #include "socklib.h"
 #include "defer.h"
 
 void do_client() {
-	const float max_timeout = 5.0f;
-	// Create a UDP client
 
-	// Step 1: Create a socket. Use DGRAM to make
-	// a UDP socket, instead of STREAM (which we
-	// used to make TCP sockets)
+	// Using UDP
+
+	// Step 1: Make a socket. Use DGRAM instead of STREAM to
+	// make a UDP socket instead of a TCP socket.
 	Socket udp_sock(Socket::Family::INET, Socket::Type::DGRAM);
-
-	// And... That's it! Now we can send and receive!
-
-	std::string msg_to_send("Hi there, server!");
-
-	float wait_time = 1.0f;
+	float wait_time = 2.0f;
 	udp_sock.SetTimeout(wait_time);
 
-	// Send data to 127.0.0.1, which always means
-	// "this machine."
-	Address srv_addr("127.0.0.1", 54345);
-	// Use sendto() instead of send() with UDP sockets.
-	// Must also provide desination address.
+	// That's it actually
+	// Now we can send and recv :)
 
-	int nbytes_recvd = -1;
+	std::string msg_to_send("Hello, server! How are you?");
+	// Send to this computer, which "127.0.0.1" always refers to.
+	Address server_addr("127.0.0.1", 10245);
+
+	// SendTo() takes an extra parameter as compared to Send():
+	// the address to which the data should be sent.
+	// The same socket can send data to any address, unlike
+	// with TCP, where a socket is bound a single connection.
+	size_t nbytes_sent = udp_sock.SendTo(msg_to_send.data(),
+		msg_to_send.size(),
+		server_addr);
+
+	std::cout << "Sent " << nbytes_sent << " bytes to "
+		<< server_addr << "\n";
+
 	char buffer[4096];
-	Address from_addr;
-	while (nbytes_recvd == -1) {
-		size_t nbytes_sent = udp_sock.SendTo(
-			msg_to_send.data(), msg_to_send.size(),
-			srv_addr);
-		std::cout << "Sent " << nbytes_sent << " bytes to " << srv_addr
-			<< ".\n";
+	Address msg_from;
 
-		nbytes_recvd = udp_sock.RecvFrom(buffer,
-			sizeof(buffer), from_addr);
+	// RecvFrom returns the number of bytes received
+	// and creates a new address, which it stores in the
+	// last parameter. This address is the address from
+	// which the data arrived.
+	const float max_wait_time = 5.0f;
+	int nbytes_recvd = -1;
+	while (wait_time < max_wait_time && nbytes_recvd == -1)
+	{
+		int nbytes_recvd = udp_sock.RecvFrom(buffer, sizeof(buffer), msg_from);
 
 		if (nbytes_recvd == -1)
 		{
-			// RecvFrom failed! Need to check why.
-			if (udp_sock.GetLastError() == Socket::Error::SOCKLIB_ETIMEDOUT) {
-				std::cout << "Timed out after " << wait_time << " seconds.\n";
-				wait_time *= 2;
-				if (wait_time > max_timeout)
-				{
-					std::cout << "Gave up after " << wait_time << " seconds.\n";
-					return;
-				}
-				else
-				{
-					std::cout << "Retrying with wait_time = " << wait_time << "...\n";
-					udp_sock.SetTimeout(wait_time);
-				}
-			}
-			else
+			// An error occurred in our receive!
+			if (udp_sock.GetLastError() == Socket::Error::SOCKLIB_ETIMEDOUT)
 			{
-				std::cout << "Unrecognized error.\n";
-				return;
+				std::cout << "Timed out. Maybe retrying...\n";
+				// We timed out! Increase the timeout time and try again.
+				wait_time *= 2;
+				udp_sock.SetTimeout(wait_time);
 			}
 		}
 	}
 
-	// Always have to check RecvFrom's return value.
-	// -1 means something went wrong!
-
-	// Try "exponential backoff" -- retry if failed, doubling
-	// the wait time. Give up after some predetermined
-	// max_timeout threshold is reached.
-
-	std::string str_msg_recvd(buffer, nbytes_recvd);
-	std::cout << "Received " << nbytes_recvd << " from " << from_addr
-		<< ": '" << str_msg_recvd << "'.\n";
-}
-
-void do_server() {
-	// Again, make a socket:
-	Socket udp_sock(Socket::Family::INET, Socket::Type::DGRAM);
-
-	// For the sever, bind() so that others
-	// know what port to send data to.
-
-	// Recall, "0.0.0.0" => Accept connections/datagrams
-	// from any address.
-	// "127.0.0.1" => Accept connections only from
-	// processes on this machine.
-	Address my_addr("0.0.0.0", 54345);
-	udp_sock.Bind(my_addr);
-
-	// No need to call Listen(), because we're not
-	// actually accepting connections -- just
-	// datagrams.
-
-	// Server needs to stay alive for client's
-	// recvfrom() to not exit with an error.
-	while (true) {
-		char buffer[4096];
-		Address from_addr;
-		int nbytes_recvd = udp_sock.RecvFrom(buffer, sizeof(buffer),
-			from_addr);
-		std::string str_msg_recvd(buffer, nbytes_recvd);
-		std::cout << "Received " << nbytes_recvd << " bytes from "
-			<< from_addr << ": '" << str_msg_recvd << "'.\n";
+	if (wait_time > max_wait_time)
+	{
+		std::cout << "Never received a message!! Now I really AM sad :(\n";
+		return;
 	}
 
-	std::string msg_to_client("Hello there, General Kenobi!");
-	/*size_t nbytes_sent = udp_sock.SendTo(
-		msg_to_client.data(), msg_to_client.size(),
-		from_addr);
-	std::cout << "Sent " << nbytes_sent << " bytes.\n"; */
+	std::cout << "Received " << nbytes_recvd << " bytes from " << msg_from
+		<< ": '";
+	std::cout.write(buffer, nbytes_recvd);
+	std::cout << "'\n";
+}
+
+void do_server()
+{
+	// As usual, make a socket
+	Socket udp_sock(Socket::Family::INET, Socket::Type::DGRAM);
+
+	// Now bind to a port so other devices know where to send their data.
+	// Recall that "0.0.0.0" => Receive connections/datagrams from anywhere
+	// and "127.0.0.1" => Only accept data from this machine (i.e., local
+	// processes).
+	Address my_address("0.0.0.0", 10245);
+	udp_sock.Bind(my_address);
+
+	// Now we're ready to receive data!
+	char buffer[4096];
+	Address from_address;
+	size_t nbytes_recvd = udp_sock.RecvFrom(buffer, sizeof(buffer), from_address);
+	std::cout << "Received " << nbytes_recvd << " bytes from " << from_address
+		<< ": '";
+	std::cout.write(buffer, nbytes_recvd);
+	std::cout << "'.\n";
+
+	std::string msg_to_send("Hello! I'm so sad :)");
+	//size_t nbytes_sent = udp_sock.SendTo(msg_to_send.data(), msg_to_send.size(), from_address);
+	//std::cout << "Sent " << nbytes_sent << " bytes to " << from_address << ".\n";
+}
+
+void generate_random_number() {
+	int num = rand();
+	std::cout << "Picked " << num << "\n.";
 }
 
 int main(int argc, char *argv[]) {
 	SockLibInit();
 	defer _([]() {SockLibShutdown();});
 
-	// If we don't receive any command line
-	// arguments, run the client; otherwise,
-	// run the server.
+	// Do this ONCE per program:
+	srand(time(NULL));
+	generate_random_number();
+	return 0;
+
+	// Run client if no args passed on command line;
+	// otherwise, run server.
 	if (argc == 1) do_client();
 	else do_server();
 
